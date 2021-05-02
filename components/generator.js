1faab129c0048e9b9cef8b12dd2afb584a858dc7
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2021-05-02 14:21:10 -0400

    Add files via upload

--- components/generator.js
+++ components/generator.js
@@ -346,7 +346,7 @@
                     var tpBackStopperId = Entities.addEntity({                
                         "type": "Box",
                         "locked": true,
-                        "visible": true,
+                        "visible": false,
                         "name": "PORTAL_BACK_STOPPER",
                         "dimensions": {
                             "x": 0.5,
