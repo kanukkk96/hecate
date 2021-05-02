7794540ee3c98f5a3f7763ebd9be67c976029186
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2021-05-02 15:32:04 -0400

    Add files via upload

--- components/generator.js
+++ components/generator.js
@@ -370,8 +370,8 @@
                             "y": 0.5,
                             "z": 0.01
                         },
-                        "rotation": Quat.fromVec3Radians( {"x": 0.0, "y": -angleRad + Math.PI, "z": 0.0} ),
-                        "position": Vec3.sum(positionZero, Vec3.multiplyQbyV(Quat.fromVec3Radians( {"x": 0.0, "y": -angleRad, "z": 0.0} ),{"x": -2.0, "y": 0.6, "z": 0.0})),
+                        "rotation": Quat.fromVec3Radians( {"x": (Math.PI/2), "y": (Math.PI/2) - angleRad, "z": 0.0} ),
+                        "position": Vec3.sum(positionZero, Vec3.multiplyQbyV(Quat.fromVec3Radians( {"x": 0.0, "y": -angleRad, "z": 0.0} ),{"x": -2.0, "y": 0.8, "z": 0.0})),
                         "grab": {
                             "grabbable": false
                         },
