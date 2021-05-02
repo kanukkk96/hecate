db39b90d5211d9e9710d00e69b0a6e0e02c9ca7c
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2021-05-02 13:44:52 -0400

    Add files via upload

---
+++ components/back.js
@@ -0,0 +1,47 @@
+"use strict";
+//
+//  back.js
+//
+//  Created by Alezia Kurdis, May 2nd, 2021.
+//  Copyright 2021 Vircadia and contributors.
+//
+//  3d portals to go back.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+(function(){
+
+    var ROOT = Script.resolvePath('').split("back.js")[0];
+    var teleporterSoundFileUrl = ROOT + "tpsound.mp3";
+    var teleportSound;
+
+
+    function playSound() {
+        Audio.playSound(teleportSound, { volume: 0.3, localOnly: true });
+    };
+    
+    this.preload = function(entityID) {
+        teleportSound = SoundCache.getSound(teleporterSoundFileUrl);
+        if (location.canGoBack()) {
+            //effect
+        }
+    }
+
+    this.enterEntity = function(entityID) {
+        if (location.canGoBack()) {
+            playSound();
+
+            var timer = Script.setTimeout(function () {
+                location.goBack();
+            }, 3000);            
+        } 
+      
+    }; 
+    
+    this.leaveEntity = function(entityID) {
+        //do nothing.
+    };
+    
+    
+})
